b8528933808538dd9b013a61faf80f9bfcf88a3c
Pooja Pasawala <pooja@basicer.com>
2013-11-17 19:48:46 -0500

    Update birthday file

--- todo3/birthday.rb
+++ todo3/birthday.rb
@@ -1,19 +1,24 @@
 require Time
-require 'date'
+require Date
 
-def is_birthday(birthday)
+def is_birthday
 
-  puts "Enter your birthday (mm/dd)"
-  birthday = gets.chomp
-  birthday.parse()
-  birthday = strptime(gets.chomp, '%Y')
-  today = Time.now. ()
-    today_date.month
-    today_date.day
-    strptime
-  if birthday = today
-    puts "happy birthday"
+  puts "Enter your birthday (mm/dd)" 
+  birthday = strptime(gets.chomp)
+  #birthday.parse()
+
+  today = Time.new
+    # today_date.month
+    # today_date.day
+  today = (today_date.month, today_date.day)
+    #strptime
+
+  if birthday == today
+    puts "Happy Birthday!"
   else
-    puts "awesome"
+    puts "Awesome!"
   end
+
 end
+
+is_birthday
