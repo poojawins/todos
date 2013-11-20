0396e8b8d7b4d88f4a14e8e5c025927b13c29d8d
Pooja Pasawala <pooja@basicer.com>
2013-11-20 09:58:57 -0500

    update files

--- todo7/listmaker_spec.rb
+++ todo7/listmaker_spec.rb
@@ -1,6 +1,6 @@
 require_relative './listmaker'
 
-describe 'make_list' do
+describe "#make_list" do
   it "should take an array and return as an array of strings in numbered list format" do
   expect(make_list(["ich", "ni", "san"])).to eq (["1. ich", "2. ni", "3. san"])
   end
