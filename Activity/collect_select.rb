9a103d32f9b1046251b0e1cee447651c1bd91228
Pooja Pasawala <pooja@basicer.com>
2013-11-20 14:09:51 -0500

    Add files

---
+++ Activity/collect_select.rb
@@ -0,0 +1,21 @@
+Implement your own versions of collect and select
+
+def my_collect(array)
+
+
+
+
+
+end
+
+
+def my_select(array)
+
+
+
+
+
+end
+
+#CHALLENGE:
+#Implement these methods as instance methods on the array class.
---
+++ Activity/collect_select_spec.rb
@@ -0,0 +1,13 @@
+require './collect_select'
+
+describe "my_collect" do
+  it "should return" do
+    expect().my_collect {} to ()
+  end
+end
+
+describe "my_select" do
+  it "should return" do
+    expect().my_select {} to ()
+  end
+end
