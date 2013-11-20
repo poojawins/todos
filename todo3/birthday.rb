083e242e823bf353a7b836ae249621cacb2e12fd
Pooja Pasawala <pooja@basicer.com>
2013-11-20 00:05:22 -0500

    Update file with working answer

--- todo3/birthday.rb
+++ todo3/birthday.rb
@@ -1,19 +1,12 @@
-require Time
-require Date
-
 def is_birthday
-
   puts "Enter your birthday (mm/dd)" 
-  birthday = strptime(gets.chomp,'%m/%d')
-
- today = (Time.now.month).to_s + "/" + (Time.now.day).to_s
-
+  birthday = gets.chomp
+  today = (Time.now.month).to_s + "/" + (Time.now.day).to_s
   if birthday == today
     puts "Happy Birthday!"
   else
     puts "Awesome, but today's not your birthday!"
   end
-
 end
 
 is_birthday
