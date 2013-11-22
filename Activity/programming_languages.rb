f1ac23b222c287efe0c3b5486beaa38c7e7f3bc4
Pooja Pasawala <pooja@basicer.com>
2013-11-22 14:00:59 -0500

    Add answer

--- Activity/programming_languages.rb
+++ Activity/programming_languages.rb
@@ -1,6 +1,7 @@
-Organizing Nested Data
+# Organizing Nested Data
 
-I have a collection of Programming Languages.  
+# I have a collection of Programming Languages.  
+
 languages = {
   :oo => {
     :ruby => {
@@ -33,16 +34,32 @@ languages = {
   }
 }
 
-I would like you to reformat the nested structure to be of the following form
+# I would like you to reformat the nested structure to be of the following form
 
-{
-  :ruby => {
-    :style => [:oo],
-    :type => "dynamic"
-  },
-  :python => {
-    :style => [:oo],
-    :type => "dynamic"
-  }
-  etc etc
-}
+# {
+#   :ruby => {
+#     :style => [:oo],
+#     :type => "dynamic"
+#   },
+#   :python => {
+#     :style => [:oo],
+#     :type => "dynamic"
+#   }
+#   etc etc
+# }
+
+new_hash = {}
+
+languages.each do |style, style_hash|
+  #:oo => { :ruby => {:type => "interpreted"}
+  #style = :oo and style_hash = { :ruby => {:type => "interpreted"}
+  style_hash.each do |name, type_hash|
+    #name = :ruby type_hash = {:type => "interpreted"}
+    new_hash[name] ||= {}
+    new_hash[name][:style] ||= style
+    new_hash[name][:type] ||= type_hash[:type]
+  end
+end
+
+p new_hash
+
