b12daf10918ba71c5750e5686ca7b006df92287c
Pooja Pasawala <pooja@basicer.com>
2013-11-26 23:18:22 -0500

    update files

--- todo8/deli_counter.rb
+++ todo8/deli_counter.rb
@@ -1,14 +1,31 @@
+
+
 class Deli
 
-  @line
+  attr_accessor :line
 
+  def initialize(customer_name)
+    @customer_name = customer_name
+    @line = []
+  end
 
   def take_a_number(customer_name)
-
+    customer_name = (line.length + 1).to_s + " " + customer_name
+    line << customer_name
   end
 
-  def now_serving
-
-  end
+  # def now_serving
+  #   puts line[0]
+  #   line = line.shift
+  # end
 
 end
+
+my_turn = Deli.new("Pooja")
+rob_turn = Deli.new("Rob")
+other_turn = Deli.new("Other")
+puts my_turn.take_a_number("Pooja")
+puts my_turn.take_a_number("Rob")
+puts my_turn.take_a_number("Other")
+# puts my_turn.now_serving
+
---
+++ todo8/deli_counter_spec.rb
@@ -0,0 +1,26 @@
+# one instance variable: line.
+# we should be able to call "take a number" that takes a customer's name, appends their number to their name and adds them to the line.
+ 
+# The line could look like this: ["1. Ashley", "2. Steve", "3. Blake"] 
+ 
+# we should be able to call a method called "now_serving" that removes the customer who is first in line and returns their name.
+ 
+# Write a test and then write the class.
+
+require './deli_counter'
+
+
+describe Deli, "take_a_number" do
+  it "should return" do
+    expect().to eq()
+  end
+
+  it "should " do
+    expect().to eq()
+  end
+
+  it "should " do
+    expect().to eq()
+  end
+
+end
