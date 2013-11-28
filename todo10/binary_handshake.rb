25055e9cb1d0315478b44fa354660f54f61c1328
Pooja Pasawala <pooja@basicer.com>
2013-11-27 22:34:26 -0500

    Update Answer

--- todo10/binary_handshake.rb
+++ todo10/binary_handshake.rb
@@ -9,40 +9,50 @@ class SecretHandshake
   def commands
     do_array = []
     command_array = ["wink", "double blink", "close your eyes", "jump"]
-    output_array = []
-    number_string.each_char do |char|
-      output_array << char
-    end
-    output_array
-    # if i > 4
-    #   command_array.reverse
-    # end
-    if output_array[-1] == "1" 
-      do_array << command_array[0]
-    end
-    if output_array[-2] == "1" 
-      do_array << command_array[1]
-    end
-    if output_array[-3] == "1" 
-      do_array << command_array[2]
-    end
-    if output_array[-4] == "1"  
-      do_array << command_array[3]
-    end
-    # if
-    #   do_array << 0
-    # end 
+      
+      if @number_string.length > 4
+        command_array.reverse
+      end
+    
+    do_array << command_array[0] if @number_string[-1] == "1" 
+    
+    do_array << command_array[1] if @number_string[-2] == "1" 
+    
+    do_array << command_array[2] if @number_string[-3] == "1" 
+    
+    do_array << command_array[3] if @number_string[-4] == "1" 
+
+      if @number_string.length > 4
+        do_array.reverse!
+      end
+    
+    do_array
+
   end 
+
 end
 
+handshake = SecretHandshake.new("1")
+handshake.commands
 
-handshake = SecretHandshake.new["1"]
-puts handshake.commands
 handshake = SecretHandshake.new("10")
+handshake.commands
+
 handshake = SecretHandshake.new("11")
+handshake.commands
+
 handshake = SecretHandshake.new("100")
+handshake.commands
+
 handshake = SecretHandshake.new("1001")
-# handshake = SecretHandshake.new("10000")
-# handshake = SecretHandshake.new("10001")
-# handshake = SecretHandshake.new("stuff")
+handshake.commands
+
+handshake = SecretHandshake.new("10000")
+handshake.commands
+
+handshake = SecretHandshake.new("10001")
+handshake.commands
+
+handshake = SecretHandshake.new("stuff")
+handshake.commands
 
