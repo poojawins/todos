5213c6c2ee58ee16df191a5f0fd7443a431bb225
Pooja Pasawala <pooja@basicer.com>
2013-12-02 13:06:17 -0500

    Update file with answer

--- todo12/shorten_tweet.rb
+++ todo12/shorten_tweet.rb
@@ -1,39 +1,23 @@
-("to"|"two"|"too")  # "to, two, too" become '2' 
-("for"|"four")    # "for, four" become '4'
-("be")          # 'be' becomes 'b'
-("you")         # 'you' becomes 'u'
-("at")          # "at" becomes "@" 
-("and")         # "and" becomes "&"
-
 
 def shorten_tweet(tweet)
-  tweet.each do |word|
-
-    if word == /(to|two|too)/
-      puts '2'
-    elsif word == /(for|four)/
-      puts '4'
-    elsif word == /("be")/
-      puts 'b'
-    elsif word == /("you")/
-      puts 'u'
-    elsif word == /("at")/
-      puts '@'
-    elsif word == /("and")/
-      puts '&'
-    else
-      puts word
-    end
-
+  if tweet.length > 140
+    tweet.gsub!('to ', '2 ')
+    tweet.gsub!('two ', '2 ')
+    tweet.gsub!('too ', '2 ')
+    tweet.gsub!('for ', '4 ')
+    tweet.gsub!('four ', '4 ')
+    tweet.gsub!('be ', 'b ')
+    tweet.gsub!('you ', 'u ')
+    tweet.gsub!('at ', '@ ')
+    tweet.gsub!('and ', '& ')
   end
-
+  puts tweet
+  puts tweet.length
 end
 
 shorten_tweet("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
-
-# 1. "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-# 2. "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?",
-# "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
-# 3. "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
-
+shorten_tweet("OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?")
+shorten_tweet("I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.")
+shorten_tweet("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")
+shorten_tweet("who is the best at being four")
 
