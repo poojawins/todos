f0e674508113e2dd41b835bb6c54cc54a40b1193
Pooja Pasawala <pooja@basicer.com>
2013-12-05 17:02:52 -0500

    Add todo13

---
+++ todo13/version_sort.rb
@@ -0,0 +1,75 @@
+# class Array
+  
+#   def version_sort (some_array)
+#     some_array.each do |files|
+#       compare = files.match(/(\d+)/)
+#     end
+
+#   end
+
+# end
+
+
+  def version_sort
+    sort_by do |file|
+      file.scan(/\d*/).collect do |x|
+        x.hex
+      end
+    end
+  end
+
+
+filenames = [
+  "foo-1.10.2.ext",
+  "foo-1.11.ext",
+  "foo-1.3.ext",
+  "foo-1.50.ext",
+  "foo-1.8.7.ext",
+  "foo-1.9.3.ext",
+  "foo-1.ext",
+  "foo-10.1.ext",
+  "foo-10.ext",
+  "foo-100.ext",
+  "foo-13.ext",
+  "foo-2.0.0.ext",
+  "foo-2.0.1.ext",
+  "foo-2.0.ext",
+  "foo-2.007.ext",
+  "foo-2.01.ext",
+  "foo-2.012b.ext",
+  "foo-2.01a.ext",
+  "foo-2.0a.ext",
+  "foo-2.0b.ext",
+  "foo-2.1.ext",
+  "foo-25.ext",
+  "foo-6.ext",
+]
+version_sorted_filenames = [
+  "foo-1.ext",
+  "foo-1.3.ext",
+  "foo-1.8.7.ext",
+  "foo-1.9.3.ext",
+  "foo-1.10.2.ext",
+  "foo-1.11.ext",
+  "foo-1.50.ext",
+  "foo-2.0.ext",
+  "foo-2.0a.ext",
+  "foo-2.0b.ext",
+  "foo-2.0.0.ext",
+  "foo-2.0.1.ext",
+  "foo-2.01.ext",
+  "foo-2.1.ext",
+  "foo-2.01a.ext",
+  "foo-2.007.ext",
+  "foo-2.012b.ext",
+  "foo-6.ext",
+  "foo-10.ext",
+  "foo-10.1.ext",
+  "foo-13.ext",
+  "foo-25.ext",
+  "foo-100.ext",
+]
+
+my_sort = Array.new
+puts my_sort.version_sort(filenames)
+
