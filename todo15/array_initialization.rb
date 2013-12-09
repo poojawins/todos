ab0ac771d9754e8fc4295543259f3198532fee93
Pooja Pasawala <pooja@basicer.com>
2013-12-09 09:26:26 -0500

    update todo15 files

---
+++ todo15/array_initialization.rb
@@ -0,0 +1,33 @@
+prereqs: initializing, methods, blocks, yield, arrays
+
+
+# EXAMPLES:
+#   
+# array_init(2) { |i| i.to_s }    # => [ '0' , '1' ]
+# array_init { |i| i.to_s }       # => [ '0' , '1' , '2' , '3' , '4' ]
+# array_init 2                    # => [ '0' , '100' ]
+# array_init                      # => [ '0' , '100' , '200' , '300' , '400' ]
+# array_init { 'hi }              # => [ 'hi' , 'hi' , 'hi' , 'hi' , 'hi' ]
+# array_init 10 do |i|            # => [ 0 , -5 , 400 , -15 , 800 , -25 , 1200 , -35 , 1600 , -45 ]
+#   if i % 2 == 0
+#     i * 200
+#   else
+#     i * -5
+#   end
+# end
+
+
+def array_init(=5)
+
+  if block_given?
+    array = 
+
+
+  else
+    array = 
+    { |i| (i * 100).to_s }
+
+    
+  end
+end
+
