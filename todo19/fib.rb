110f26d73f55bddb36a07527167d281b8f47e271
Pooja Pasawala <pooja@basicer.com>
2013-12-13 09:29:59 -0500

    Add recursive solution

---
+++ todo19/fib.rb
@@ -0,0 +1,15 @@
+# prereqs: arrays, methods, conditional logic
+# create a method that returns the nth number of the fibonacci sequence
+# fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
+
+def fibo_finder(n)
+  
+  if n == 0
+    return 0
+  elsif n ==1
+    return 1
+  else
+    return (fibo_finder(n - 1) + fibo_finder(n - 2))
+  end
+end
+
