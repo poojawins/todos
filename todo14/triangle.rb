267ed1809dcdad01113475da5dbd8682e6568ee5
Pooja Pasawala <pooja@basicer.com>
2014-01-01 18:53:58 -0500

    Add answer

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -1,18 +1,35 @@
 class Triangle
 
-  def 
-    
+  attr_reader :x, :y, :z
+  def initialize (x, y, z)
+    @x = x
+    @y = y
+    @z = z
   end
 
+  def kind
+    if not_valid?
+      raise TriangleError
+    elsif x == y && y == z
+      :equilateral   
+    elsif x == y || y == z || z == x
+      :isosceles 
+    else 
+      :scalene
+    end
+  end
 
-if not a triangle: 
-raise_error
-TriangleError
+  def not_valid?
+    x + y <= z || y + z <= x || z + x < y
+    x <= 0 || y <= 0 || z <= 0
+  end
+
 end
 
-:equilateral
 
-:isosceles
+class TriangleError < StandardError
+end
 
-:scalene
+
+
 
--- todo14/triangle_spec.rb
+++ todo14/triangle_spec.rb
@@ -4,28 +4,20 @@
 
 # As a bonus, the program should raise an error if the triangle cannot exist.
 
-# ## Hint
+# Hint
 
-# The sum of the lengths of any two sides of a triangle always exceeds the length of the third side, a principle known as the _triangle inequality_.
+# The sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+# a principle known as the _triangle inequality_.
 
-## Building and Raising a Custom Error
-
+# Building and Raising a Custom Error
 # Read: 
 # http://ruby.about.com/od/advancedruby/qt/quickexceptions.htm
 # http://www.skorks.com/2009/09/ruby-exceptions-and-exception-handling/
 # http://phrogz.net/programmingruby/tut_exceptions.html
 # http://ablogaboutcode.com/2011/01/03/using-custom-error-messages-for-cleaner-code/
 
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
 
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
-
-  # Use the specified formatter
-  config.formatter = :progress # :progress, :html, :textmate
-end
+require_relative './triangle'
 
 describe 'Triangle' do
 
