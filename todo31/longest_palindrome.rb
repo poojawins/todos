ab902d841a372fb6f25942935114c91a273d9e86
Pooja Pasawala <pooja@basicer.com>
2014-01-08 12:34:28 -0500

    Initial commit

---
+++ todo31/longest_palindrome.rb
@@ -0,0 +1,10 @@
+# A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
+
+def longest_palindrome(given_string)
+  look_array = given_string.split
+
+
+end
+
+longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd") #=> "dhfdkjfffhhfffjkdfhd"
+
