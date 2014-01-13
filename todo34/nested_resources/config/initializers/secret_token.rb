989cbfbbc94e916563e546223b1f4e40627a4d6d
Pooja Pasawala <pooja@basicer.com>
2014-01-13 09:18:04 -0500

    Initial commit

---
+++ todo34/nested_resources/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestedResources::Application.config.secret_key_base = '610c73062a3c72d438a59f2006fce1d359282716cef745ab5e7d7cd92c86309dc43c02759389d85b0bc3095ce32e2f2fbd95f72f389edad8a3d94100ecc044dd'
---
+++ todo34/todo_34.md
@@ -0,0 +1,9 @@
+Nested Resources
+
+Create 2 resources using a scaffold, nest them.
+
+Build the create action for the nested resource.  So for example, posts have many comments, I have a form that can create a new comment for a particular post.
+
+Examples:
+Parks have many waterslides, build a form for a water slide
+You guys have many stories (that you won't tell me), build a form for a story
