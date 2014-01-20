fd53891f562e74bde103e397d7728cf11153492f
Pooja Pasawala <pooja@basicer.com>
2014-01-20 09:11:03 -0500

    Initial Commit

---
+++ todo40/seperate_numbers_with_commas.rb
@@ -0,0 +1,11 @@
+#Separate numbers with a comma each three digits.
+
+def separate_with_comma(n)
+  n.each do |digit|
+
+  end
+end
+
+separate_with_comma(1) #=> "1" 
+separate_with_comma(1000) #=> "1,000" 
+separate_with_comma(10000) #=> "10,000"
