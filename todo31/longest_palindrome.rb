9b9ad9dddf6ce2f0280edee184249da977b7a104
Pooja Pasawala <pooja@basicer.com>
2015-02-18 00:55:46 -0500

    Add longest_palindrome answer

--- todo31/longest_palindrome.rb
+++ todo31/longest_palindrome.rb
@@ -1,10 +1,17 @@
-# A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
+# A palindrome is a string that is written the same forward as it is in reverse. 
+#  Write a method to return the longest palindrome in a given string
 
 def longest_palindrome(given_string)
-  look_array = given_string.split
-
+  palindrome = ""
+  (0...given_string.length).each do |i|
+    (0...given_string.length).each do |j|
+      if given_string[i, j] == given_string[i, j].reverse && given_string[i, j].length > palindrome.length
+        palindrome = given_string[i, j]
+      end
+    end
+  end
+  puts palindrome
 
 end
 
 longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd") #=> "dhfdkjfffhhfffjkdfhd"
-
