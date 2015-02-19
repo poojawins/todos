a77615b7de9293bae1de590b58b562dd9c02321f
Pooja Pasawala <pooja@basicer.com>
2015-02-18 23:53:05 -0500

    Add answer for separating numbers with commas

--- todo40/seperate_numbers_with_commas.rb
+++ todo40/seperate_numbers_with_commas.rb
@@ -1,10 +1,21 @@
 #Separate numbers with a comma each three digits.
 
 def separate_with_comma(n)
-  n.to_s.reverse.gsub(/(\d{3})(\d+)(\d*)/, $1",").reverse
+  n = n.to_s.reverse.split(//)
+  new_string = ""
+  n.each_with_index do |num, i|
+    if i % 3 == 0 && i != 0
+      new_string = new_string + ",#{num}"
+    else
+      new_string += num
+    end
+    i += 1
+  end
+  puts new_string.reverse
 end
 
-puts separate_with_comma(1)
-puts separate_with_comma(1000)
-puts separate_with_comma(10000)
-
+separate_with_comma(1) # 1
+separate_with_comma(1000) # 1,000
+separate_with_comma(1111) # 1,111
+separate_with_comma(10000) # 10,000
+separate_with_comma(1000000) # 1,000,000
